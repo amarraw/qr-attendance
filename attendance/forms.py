71e8c4375ce7552b7a2135552c1e14ce9529916c
amarraw <amar.raw011@gmail.com>
2025-12-27 07:12:58 +0545

    full html

--- attendance/forms.py
+++ attendance/forms.py
@@ -4,16 +4,39 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Student, AdminProfile, AttendanceSession
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column, Div
+from crispy_forms.layout import Layout, Submit, Row, Column, Field
 
 class StudentRegistrationForm(UserCreationForm):
-    # student_id = forms.CharField(max_length=20, required=True)
-    department = forms.CharField(max_length=100, required=True)
-    year = forms.IntegerField(min_value=1, max_value=5, required=True)
-    email = forms.EmailField(required=True)
-    phone = forms.CharField(max_length=15, required=False)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    department = forms.CharField(
+        max_length=100, 
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'e.g., Computer Science'})
+    )
+    year = forms.IntegerField(
+        min_value=1, 
+        max_value=5, 
+        required=True,
+        widget=forms.NumberInput(attrs={'placeholder': 'e.g., 3'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+    )
+    phone = forms.CharField(
+        max_length=15, 
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Optional phone number'})
+    )
+    first_name = forms.CharField(
+        max_length=30, 
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'First name'})
+    )
+    last_name = forms.CharField(
+        max_length=30, 
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Last name'})
+    )
     
     class Meta:
         model = User
@@ -23,7 +46,32 @@ class StudentRegistrationForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Register'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-4'
+        self.helper.field_class = 'col-md-8'
+        self.helper.layout = Layout(
+            Row(
+                Column('username', css_class='form-group col-md-6'),
+                Column('student_id', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('first_name', css_class='form-group col-md-6'),
+                Column('last_name', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('email', css_class='form-group col-md-6'),
+                Column('phone', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('department', css_class='form-group col-md-6'),
+                Column('year', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('password1', css_class='form-group col-md-6'),
+                Column('password2', css_class='form-group col-md-6'),
+            ),
+            Submit('submit', 'Register', css_class='btn-primary')
+        )
     
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -43,14 +91,51 @@ class StudentRegistrationForm(UserCreationForm):
         return user
 
 class AdminRegistrationForm(UserCreationForm):
-    department = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    department = forms.CharField(
+        max_length=100, 
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your department'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+    )
+    first_name = forms.CharField(
+        max_length=30, 
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'First name'})
+    )
+    last_name = forms.CharField(
+        max_length=30, 
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Last name'})
+    )
     
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-4'
+        self.helper.field_class = 'col-md-8'
+        self.helper.layout = Layout(
+            'username',
+            Row(
+                Column('first_name', css_class='form-group col-md-6'),
+                Column('last_name', css_class='form-group col-md-6'),
+            ),
+            'email',
+            'department',
+            Row(
+                Column('password1', css_class='form-group col-md-6'),
+                Column('password2', css_class='form-group col-md-6'),
+            ),
+            Submit('submit', 'Register', css_class='btn-success')
+        )
     
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -65,20 +150,40 @@ class AdminRegistrationForm(UserCreationForm):
         return user
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+            'class': 'form-control'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form-control'
+        })
+    )
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Login', css_class='btn-primary btn-block'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-4'
+        self.helper.field_class = 'col-md-8'
+        self.helper.layout = Layout(
+            'username',
+            'password',
+            Submit('submit', 'Login', css_class='btn-primary btn-block')
+        )
 
 class AttendanceSessionForm(forms.ModelForm):
     class Meta:
         model = AttendanceSession
         fields = ['name', 'course_code', 'session_type', 'start_time', 'end_time', 'location']
         widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter session name'}),
+            'course_code': forms.TextInput(attrs={'placeholder': 'e.g., CS101'}),
+            'location': forms.TextInput(attrs={'placeholder': 'e.g., Room 201'}),
             'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
@@ -87,7 +192,20 @@ class AttendanceSessionForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Create Session'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-4'
+        self.helper.field_class = 'col-md-8'
+        self.helper.layout = Layout(
+            'name',
+            'course_code',
+            'session_type',
+            Row(
+                Column('start_time', css_class='form-group col-md-6'),
+                Column('end_time', css_class='form-group col-md-6'),
+            ),
+            'location',
+            Submit('submit', 'Create Session', css_class='btn-primary')
+        )
 
 class QRScanForm(forms.Form):
     qr_data = forms.CharField(widget=forms.HiddenInput())
@@ -97,3 +215,4 @@ class QRScanForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
+        self.helper.form_class = 'form-horizontal'
